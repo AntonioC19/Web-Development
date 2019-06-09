df4d2e293336acd010a3bacef18e1c049d182ed0
Antonio <lorenzo19anthony@gmail.com>
2019-06-08 18:46:23 -0600

    Jquery
    Learning the basics of Jquery.

--- Javascript-game/javascript-home.js
+++ Javascript-game/javascript-home.js
@@ -1,11 +1,37 @@
 window.onload = function(){
-	document.getElementById("button").onclick = function() {
-		var randomNumber = Math.random();
-		randomNumber = randomNumber * 6;
-		randomNumber = Math.floor(randomNumber);
+	var start = new Date().getTime();
 
-		if(document.getElementById("guess").value == randomNumber){
-			alert("you got it!");
-		}else alert("nope! the number was " + randomNumber);	
+	function getRandomColor() {
+	  var letters = '0123456789ABCDEF'.split('');
+	  var color = '#';
+	  for (var i = 0; i < 6; i++) {
+	    color += letters[Math.floor(Math.random() * 16)];
+	  }
+	  return color;
 	}
+
+	function shapeAppear(){
+		var randomTop = Math.random() * 400;
+		var randomWidth = (Math.random() * 200) + 100;
+		document.getElementById("shape").style.background = getRandomColor();
+		document.getElementById("shape").style.width = randomWidth + "px";
+		document.getElementById("shape").style.top = randomTop + "px";
+		document.getElementById("shape").style.left = randomTop + "px";
+		document.getElementById("shape").style.display = "block";
+		start = new Date().getTime();
+	}
+
+	function appearDelay(){
+		setTimeout(shapeAppear, Math.random() * 2000);
+	}
+	appearDelay();
+
+	document.getElementById("shape").onclick = function(){
+		document.getElementById("shape").style.display = "none";
+		var end = new Date().getTime();
+		var timeTaken = (end - start) / 1000;
+		document.getElementById("timeTaken").innerHTML = timeTaken + "s";
+		appearDelay();
+	}
+
 }
